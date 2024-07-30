f0f6c1186f138cefbc84fe2b7cab74a8ad679280
dmillions <derek.m360@gmail.com>
2024-07-30 13:29:05 -0400

    game now tracks who wins deleted commetns

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import Player from "./components/player";
 import GameBoard from "./components/gameBoard";
 import Log from "./components/log";
+import { WINNING_COMBINATIONS } from "./winning-combos";
+
+const intialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -16,12 +23,36 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState("X");
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = intialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combos of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combos[0].row][combos[0].column];
+    const secondSquareSymbol = gameBoard[combos[1].row][combos[1].column];
+    const thirdSquareSymbol = gameBoard[combos[2].row][combos[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+      console.log(winner);
+    }
+  }
+
   function handleSelectSquare(rowIndex, colIndex) {
-    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
       let currentPlayer = deriveActivePlayer(prevTurns);
 
@@ -52,7 +83,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>you won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/log.jsx
+++ src/components/log.jsx
@@ -3,7 +3,6 @@ export default function Log({ turns }) {
     <ol id="log">
       {turns.map((turn) => (
         <li>
-          key={`${turn.square.row}${turn.square.col}`}
           {turn.player} selected {turn.square.row},{turn.square.col}{" "}
         </li>
       ))}
