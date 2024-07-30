12a36810cba1dc0c9af0bb16d2ac0a540864d959
dmillions <derek.m360@gmail.com>
2024-07-30 12:38:00 -0400

    board and players now reflect turns and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 import Player from "./components/player";
 import GameBoard from "./components/gameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player intialName="Player 1" symbol="X" />
-          <Player intialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            intialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            intialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       log
     </main>
