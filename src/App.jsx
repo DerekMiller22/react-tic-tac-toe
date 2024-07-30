d1e999825692b3d045d8ff47bfc01e39c1bc7e93
dmillions <derek.m360@gmail.com>
2024-07-30 13:07:29 -0400

    changed how state is derived and disabled clicked buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import Player from "./components/player";
 import GameBoard from "./components/gameBoard";
 import Log from "./components/log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
+      let currentPlayer = deriveActivePlayer(prevTurns);
 
       if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "0";
+        currentPlayer = "O";
       }
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
