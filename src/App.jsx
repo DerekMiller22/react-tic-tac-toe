6dd177a2304878fa5ed8f2ab5ea4368da20d8dc8
dmillions <derek.m360@gmail.com>
2024-07-30 13:00:45 -0400

    game board and log now switch turns and track board state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from "react";
 
 import Player from "./components/player";
 import GameBoard from "./components/gameBoard";
+import Log from "./components/log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "0";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -25,12 +40,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      log
+      <Log turns={gameTurns} />
     </main>
   );
 }
